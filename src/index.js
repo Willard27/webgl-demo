a9cd41139ac0eefba6bb09340df9143a69f891bf
Willard27 <singlecycle27@163.com>
2024-07-07 17:26:10 +0800

    feat: 生成随机颜色的点

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import frag from './shader/index.frag';
 import vert from './shader/index.vert';
-import { createProgram, createShader } from './util';
+import { createProgram, createShader, randomColor } from './util';
 
 var canvas = document.querySelector('#canvas')
 // 兼容性判断
@@ -33,11 +33,51 @@ const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, frag);
 // gl.linkProgram(program);
 
 const program = createProgram(gl, vertexShader, fragmentShader)
-
 gl.useProgram(program);
+
+const point = [];
+
+const a_position = gl.getAttribLocation(program, 'a_position');
+const a_screen_size = gl.getAttribLocation(program, 'a_screen_size');
+const u_color = gl.getUniformLocation(program, 'u_color');
+
+gl.vertexAttrib2f(a_screen_size, canvas.width, canvas.height);
+
+canvas.addEventListener('click', function (e) {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const color = randomColor();
+
+    point.push({
+        x,
+        y,
+        color
+    })
+
+    // 清空画布
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // 传入上一次的颜色
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    for (let i = 0; i < point.length; i++) {
+        const color = point[i].color;
+
+
+        gl.uniform4f(u_color, color.r, color.g, color.b, color.a);
+
+        console.log(i)
+        gl.vertexAttrib2f(a_position, point[i].x, point[i].y);
+        // 绘制
+        gl.drawArrays(gl.POINTS, 0, 1)
+
+    }
+
+
+})
+
+
+// 清空画布
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 // 传入上一次的颜色
 gl.clear(gl.COLOR_BUFFER_BIT);
-
-// 绘制
-gl.drawArrays(gl.POINTS, 0, 1);
--- src/util/index.js
+++ src/util/index.js
@@ -18,3 +18,12 @@ export const createProgram = function (gl, vertexShader, fragmentShader) {
     return program
 
 }
+
+export const randomColor = function () {
+    return {
+        r: Math.random() * 255,
+        g: Math.random() * 255,
+        b: Math.random() * 255,
+        a: 1
+    }
+}
